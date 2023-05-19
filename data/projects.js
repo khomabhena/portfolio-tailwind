1bbd9c0e32434616e3daf73620c5eea96f2ea913
khomabhena <colwanymab@gmail.com>
2023-05-19 12:32:55 +0200

    Updated project descriptions

--- data/projects.js
+++ data/projects.js
@@ -12,9 +12,9 @@ const getProjects = () => {
         techMore: 'React / Next JS',
         liveDemo: 'https://uk-care-connection.vercel.app',
         gitHubLink: 'https://github.com/khomabhena/ukc-tailwind',
-        desc: placeholder,
+        desc: 'It’s a platform for Care Workers to find jobs in the UK and for employers to source talent for various open positions. It was built using React and then later on migrated to Next JS and Tailwind to improve page loading times and it’s hosted on Firebase. The platform employs the Stripe Payment Service to handle subscriptions.',
         techStack: [
-            'React', 'Next JS', 'Tailwind CSS'
+            'React', 'Next JS', 'Tailwind CSS', 'Firebase', 'Stripe'
         ]
     },
     {
@@ -26,9 +26,9 @@ const getProjects = () => {
         techMore: 'React / Next JS',
         liveDemo: 'https://khomab-shop.vercel.app',
         gitHubLink: 'https://github.com/khomabhena/ecommerce',
-        desc: placeholder,
+        desc: 'Designed as an e-commerce service to sell equipment and other phone accessories. The service was developed using Next JS to utilise the Server Rendering function for faster loading times of product pages and give a seamless experience to customers. The products are handled and managed on the Sanity CMS engine for easier product and store management without involving engineers. Product payments are facilitated by the Stripe platform.',
         techStack: [
-            'React', 'Next JS', 'Tailwind CSS'
+            'React', 'Next JS', 'Sanity CMS', 'Stripe'
         ]
     },
     {
@@ -40,9 +40,9 @@ const getProjects = () => {
         techMore: 'React / Next JS',
         liveDemo: 'https://uk-care-connection.vercel.app/applicant/signin',
         gitHubLink: 'https://github.com/khomabhena/ukc-tailwind',
-        desc: placeholder,
+        desc: 'It’s a platform for Care Workers to find jobs in the UK and for employers to source talent for various open positions. It was built using React and then later on migrated to Next JS and Tailwind to improve page loading times and it’s hosted on Firebase. The platform employs the Stripe Payment Service to handle subscriptions.',
         techStack: [
-            'React', 'Next JS', 'Tailwind CSS'
+            'React', 'Next JS', 'Tailwind CSS', 'Stripe'
         ]
     },
     {
@@ -54,7 +54,7 @@ const getProjects = () => {
         techMore: 'Next JS / Tailwind CSS',
         liveDemo: 'https://khomabhena.vercel.app',
         gitHubLink: 'https://github.com/khomabhena/portfolio-tailwind',
-        desc: placeholder,
+        desc: 'This is my personal portfolio, developed using Next JS and Tailwind and hosted locally.',
         techStack: [
             'React', 'Next JS', 'Tailwind CSS'
         ]
@@ -68,9 +68,9 @@ const getProjects = () => {
         techMore: 'React / Next JS',
         liveDemo: 'https://imali-app.vercel.app',
         gitHubLink: 'https://github.com/khomabhena/imali-tailwind',
-        desc: placeholder,
+        desc: 'This service is designed to allow individuals to manage their income and spend wisely. The system employs complex algorithms which make sure that every cent is accounted for and one doesn’t spend more than one can afford. Your income will be allocated to various buckets and you can then spend according to the bucket allocation. The platform is developed using Next JS and Tailwind and plans are to host it on MongoDB.',
         techStack: [
-            'React', 'Next JS', 'Tailwind CSS'
+            'React', 'Next JS', 'Tailwind CSS', 'Firebase'
         ]
     },
     {
@@ -82,9 +82,9 @@ const getProjects = () => {
         techMore: 'React / Next JS',
         liveDemo: 'https://daylong.vercel.app',
         gitHubLink: 'https://github.com/khomabhena/daylong',
-        desc: placeholder,
+        desc: 'Daylong is a system that helps with day management for freelancers so that they can get more work done on a single project and also be able to work on various projects during the day. It also allows a freelancer to dedicate some time to upskilling and learning new things in their field of work.',
         techStack: [
-            'React', 'Next JS', 'Tailwind CSS'
+            'React', 'Next JS', 'Tailwind CSS', 'MongoDB'
         ]
     }
   ]
