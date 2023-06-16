aaf34e547f1b1e2bece076678fdfa2512e065b91
khomabhena <colwanymab@gmail.com>
2023-06-16 16:02:01 +0200

    Summarized the about me section

--- components/About.jsx
+++ components/About.jsx
@@ -5,10 +5,15 @@ import AboutImg from '../public/assets/about.jpg';
 import Title from './Title';
 
 const About = () => {
-  const one = "I specialize in developing responsive web apps using technologies such as React, Next JS, JavaScript, Java and Tailwind. I use Node JS and Express to connect to the backend or use Next JS's inbuilt API routing service. I’m proficient in using databases such as SQL and non-relational databases such as MongoDB and Firebase along with other mobile-friendly databases such as SQLite."
-  const two = "I started programming back in 2013 developing single-page web apps using JavaScript, JQuery, PHP,  MySQL, HTML, CSS and AJAX. I then transitioned into developing Android apps using JAVA. I built quite a number of apps as a freelancer and helped college students with their projects."
-  const three = "I was once a part-time tutor at a private Senior School teaching programming fundamentals in JAVA and C#."
+  // const one = "I specialize in developing responsive web apps using technologies such as React, Next JS, JavaScript, Java and Tailwind. I use Node JS and Express to connect to the backend or use Next JS's inbuilt API routing service. I’m proficient in using databases such as SQL and non-relational databases such as MongoDB and Firebase along with other mobile-friendly databases such as SQLite."
+  // const two = "I started programming back in 2013 developing single-page web apps using JavaScript, JQuery, PHP,  MySQL, HTML, CSS and AJAX. I then transitioned into developing Android apps using JAVA. I built quite a number of apps as a freelancer and helped college students with their projects."
+  // const three = "I was once a part-time tutor at a private Senior School teaching programming fundamentals in JAVA and C#."
   
+
+  const one = "Proficient in developing responsive web apps using React, Next JS, JavaScript, and Tailwind."
+  const two = "Skilled in connecting to the backend using Node JS and Express or Next JS's inbuilt API routing service."
+  const three = "Experienced in working with SQL and non-relational databases such as MongoDB and Firebase, as well as mobile-friendly databases such as SQLite."
+
   return (
     <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
