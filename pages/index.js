e29d9c8df103409d78d429b7dffb74e0da69a112
khomabhena <colwanymab@gmail.com>
2023-12-30 01:47:25 +0200

    Removed the contact section

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
         <Technologies />
         <Skills />
         <Projects />
-        <Contact />
+        {/* <Contact /> */}
       </div>
     </div>
   )
