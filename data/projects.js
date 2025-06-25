d195c8ff4063be990e20845c57031eafd43bfa0a
Kholwani Mabhena <colwanymab@gmail.com>
2025-06-25 14:01:15 +0200

    added the appletree project to the list

--- data/projects.js
+++ data/projects.js
@@ -4,6 +4,20 @@ const getProjects = () => {
     const placeholder = 'This app was built using React JS and is hosted on Firebase. Users are able to search properties based on an Address, City, or ZIP code to retrieve a list of active properties currently for sale. You will be able to view property information as well as the specific location of the property integrated with the Google Maps API. User authentication is available so you can signup and signin to your account with an email address in order to save your favorite properties. This is made possible with Zillow API.'
   return [
     {
+        id: 'appletree',
+        title: 'Apple Tree',
+        image: '/assets/projects/project-appletree.png',
+        link: '/projects/appletree',
+        tech: 'ReactJS',
+        techMore: 'ReactJS / TailwindCSS',
+        liveDemo: 'https://appletree-mdu.vercel.app',
+        gitHubLink: 'https://github.com/khomabhena/appletree',
+        desc: 'This UI was designed using Vite, React and TailwindCSS',
+        techStack: [
+            'React', 'Vite', 'Tailwind CSS',
+        ]
+    },
+    {
         id: 'ukc',
         title: 'UK Care Connection',
         image: '/assets/projects/project-ukc.PNG',
